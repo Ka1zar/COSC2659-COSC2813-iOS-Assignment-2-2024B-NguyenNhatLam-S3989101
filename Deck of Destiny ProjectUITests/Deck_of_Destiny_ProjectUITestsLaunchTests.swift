ef1866d19ae7335b52c0a474ecce868a34cf47a4
Ka1zar <118871898+Ka1zar@users.noreply.github.com>
2024-09-04 13:32:29 +0800

    Add files via upload

---
+++ Deck of Destiny ProjectUITests/Deck_of_Destiny_ProjectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Deck_of_Destiny_ProjectUITestsLaunchTests.swift
+//  Deck of Destiny ProjectUITests
+//
+//  Created by user265979 on 9/4/24.
+//
+
+import XCTest
+
+final class Deck_of_Destiny_ProjectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
